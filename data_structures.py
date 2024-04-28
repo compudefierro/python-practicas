75a63ff579ccf1cc7bebe6b6f7e0e6336c0ed9c5
compudefierro <jorgeeldri@gmail.com>
2024-04-28 17:30:45 -0300

    data structures practice

--- data_structures.py
+++ data_structures.py
@@ -39,3 +39,23 @@ print(transposed)
 transposed = []
 transposed = list(zip(*matrix))
 print(transposed)
+
+# Tuples and Sequences
+tuple_nested = ([1,2,3],[3,2,1])
+print(tuple_nested, type(tuple_nested))
+tuple_nested[0].append(4)
+tuple_nested[1].insert(0,4)
+print(tuple_nested, type(tuple_nested))
+
+# Sets
+basket = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
+print(basket)
+
+# Demonstrate set operations on unique letters from two words
+set_a = set('abracadabra')
+set_b = set('alacazam')
+print(f'letters in a but not in b: \t{set_a - set_b} \nletters in a or b or both: \t{set_a | set_b}\nletters in both a and b: \t{set_a & set_b}\nletters in a or b but not both: {set_a ^ set_b}')
+
+# Set: list comprehensions:
+set_comp_a = {x for x in 'abracadabra' if x not in 'abc'}
+print(set_comp_a)
